aae56e253e7050f928339bdcb24b89c02b3fff69
asanlu <1925751694@qq.com>
2018-04-23 18:40:07 +0800

    add edit&postcss config

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,9 +2,17 @@
 
 module.exports = {
   "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
+    // "postcss-import": {}, 
+    // "postcss-url": {},    
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      // "browsers": [
+      //   "ie >= 9",
+      //   "ff >= 30",
+      //   "chrome >= 34",
+      //   "safari >= 7",
+      //   "opera >= 23"
+      // ]
+    }
   }
 }
